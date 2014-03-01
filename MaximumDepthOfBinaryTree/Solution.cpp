f7ac7b121eaf69766d251e3d3edd8207784f987b
Enrique A. Tobis <enrique@tobis.com.ar>
2014-02-28 22:30:33 -0500

    Maximum Depth of Binary Tree

---
+++ MaximumDepthOfBinaryTree/Solution.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+
+class Solution {
+public:
+  int maxDepth(TreeNode *root) {
+    if (!root) {
+      return 0;
+    } else {
+      return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+  }
+};
---
+++ MaximumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return Math.max(maxDepth(root.right), maxDepth(root.left)) + 1;
+        }
+    }
+}
