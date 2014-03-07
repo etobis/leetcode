097a1cad8e2f0c4901b4d307b9f2e118cc56de07
Enrique A. Tobis <enrique@tobis.com.ar>
2014-03-06 23:33:29 -0500

    Fix a bug

--- SameTree/Solution.cpp
+++ SameTree/Solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
   bool isSameTree(TreeNode *p, TreeNode *q) {
-    return (p == null) == (q == null) &&
-      (p == null || (p->val == q->val && isSameTree(p->left, q->left) && 
+    return (p == 0) == (q == 0) &&
+      (p == 0 || (p->val == q->val && isSameTree(p->left, q->left) && 
 		     isSameTree(p->right, q->right)));
   }
 };
